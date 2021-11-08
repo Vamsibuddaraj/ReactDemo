c99972324e643bf1ea35d1e8295c783e42d77839
Vamsibuddaraj <vamsibuddaraju11&gmail.com>
2021-11-08 15:01:44 +0530

    Passing properties to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <Greet/>
+      <Greet name="vamsi" college="gitam"><p> this is children property</p></Greet>
+      <Greet name = "krishnam" college = "gitam"/>
+    <Greet name ="raju"/>
+    <Welcome/>
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
-function Greet(){
+/*function Greet(){
     return <h1>Welcome Vamsi</h1>
+}*/
+
+//const Greet=()=> <h1>Hello varma</h1>
+
+const Greet = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <h1> hello  {props.name} you are from {props.college}</h1>
+            {props.children}
+        </div>
+    )
+
 }
 
 export default Greet
