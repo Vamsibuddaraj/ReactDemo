d461869f86f80aa6c6ffb58832a41aae8977a7ad
Vamsibuddaraj <vamsibuddaraju11&gmail.com>
2021-11-09 16:07:22 +0530

    conditional rendering--if/else

---
+++ src/components/UserGreeting.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class UserGreeting extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             isLoggedIn : true
+        }
+    }
+    
+    render() {
+        if(this.state.isLoggedIn){
+            return <div>welcome vamsi</div>
+        }
+        else{
+            return <div>welcome guest</div>
+        }
+      
+    }
+}
+
+export default UserGreeting
