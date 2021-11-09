f6527db13d039c618c8685f57195304922476e65
Vamsibuddaraj <vamsibuddaraju11&gmail.com>
2021-11-09 12:03:04 +0530

    event binding

--- src/App.js
+++ src/App.js
@@ -6,24 +6,12 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
-      <FunctionClick/>
-      <ClassClick/>
-
-      <Message/>
-
-      <Greet name = "vamsi"  college = "IIT">
-      <p> this is children property</p>
-      </Greet>
-<     Greet name = "krishnam" college = "Oxford">
-        <button>click me</button>
-      </Greet>
-      <Greet />
-      <Welcome name ="raju" alias = "varma"/>
+      <EventBind/>
       </div>
     );
 }
