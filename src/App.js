7367758acde09cf73e781c711deba1f39fee3011
Vamsibuddaraj <vamsibuddaraju11&gmail.com>
2021-11-09 09:51:05 +0530

    handling state change

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
+      <Message/>
       <Greet name="vamsi" college="gitam"><p> this is children property</p></Greet>
-      <Greet name = "krishnam" college = "gitam"/>
-    <Greet name ="raju"/>
-    <Welcome/>
+      <Greet name = "ansari" college = "Oxford">
+      <button>click me</button>
+    </Greet>
+    <Greet />
+
+    <Welcome name ="tanveer" alias = "ansari"/>
     </div>
   );
 }
