5cd58b44c8ceb859ee3c5b5ef386f5a2dbe9b847
Vamsibuddaraj <vamsibuddaraju11&gmail.com>
2021-11-09 09:57:53 +0530

    counter increment button click

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
+      <Counter/>
+
       <Message/>
-      <Greet name="vamsi" college="gitam"><p> this is children property</p></Greet>
-      <Greet name = "ansari" college = "Oxford">
-      <button>click me</button>
-    </Greet>
-    <Greet />
 
-    <Welcome name ="tanveer" alias = "ansari"/>
-    </div>
-  );
+      <Greet name = "vamsi"  college = "IIT">
+      <p> this is children property</p>
+      </Greet>
+<     Greet name = "krishnam" college = "Oxford">
+        <button>click me</button>
+      </Greet>
+      <Greet />
+      <Welcome name ="raju" alias = "varma"/>
+      </div>
+    );
 }
-
 export default App;
