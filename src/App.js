6c7b17be69661a6864b062c5622bc19defe3934a
Vamsibuddaraj <vamsibuddaraju11&gmail.com>
2021-11-18 21:02:40 +0530

    useEffect componentWillUnmount window.removeEventListener

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import HookCounter2 from './components/hooks/HookCounter2';
-import HookCounter3 from './components/hooks/HookCounter3';
-import CounterHook1 from './components/hooks/useeffect/CounterHook1';
-import ClassCounter1 from './components/hooks/useeffect/ClassCounter1';
-import HookMouse from './components/hooks/useeffect/HookMouse';
+
+import MouseContainer from './components/hooks/useeffect/MouseContainer';
 
 
 
@@ -13,7 +10,7 @@ import HookMouse from './components/hooks/useeffect/HookMouse';
 function App() {
   return (
     <div className="App">
-      <HookMouse/>
+      <MouseContainer/>
     </div>
   );
 }
