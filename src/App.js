c6029422675a6c543952af7fc477d5d8f375e6d6
Vamsibuddaraj <vamsibuddaraju11&gmail.com>
2021-11-18 20:51:37 +0530

    useEffect hook to update title

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import HookCounter2 from './components/hooks/HookCounter2';
 import HookCounter3 from './components/hooks/HookCounter3';
+import CounterHook1 from './components/hooks/useeffect/CounterHook1';
 
 
 
@@ -10,7 +11,7 @@ import HookCounter3 from './components/hooks/HookCounter3';
 function App() {
   return (
     <div className="App">
-      <HookCounter3/>
+      <CounterHook1/>
     </div>
   );
 }
